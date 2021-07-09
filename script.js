2f24ef2a7226e5716a5f945a2f6423c501f0680b
Pallavi7597 <66312599+Pallavi7597@users.noreply.github.com>
2021-07-09 12:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    result = document.getElementById("result");
-    
-    
-               
-    var interest = (p*r*y)/100;
-
-
-    
-    var amt=interest;
-    var result=document.getElementById("result");
-    result.innerHTML="If you deposit "+p+",<br> at an interest rate of "+r+"%. <br>You will receive an amount of " +amt+",<br>in the year"+year;
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
 }
 
- 
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else {
+         document.getElementById("result").innerHTML = "If you deposit \<mark>" + principal + "\</mark>,\<br\>at an interest rate of \<mark>" + rate + "%\</mark>\<br\>You will receive an amount of \<mark>" + amount + "\</mark>,\<br\>in the year \<mark>" + year + "\</mark>\<br\>";
+    }
+}
